0609b8d9de49c1178f87d31e9569dfea2366147f
Maulikdikshit <maulikdikshit013@gmail.com>
2025-01-07 19:51:14 +0530

    contextAndProp

--- api/src/App.js
+++ api/src/App.js
@@ -4,14 +4,21 @@ import "./App.css";
 import UserInfoContext from "./context/UserInfoContext";
 import BlogPage from "./components/BlogPage";
 
+import { ThemeProvider } from "./context/ThemeProvider";
+import ContentComponent from "./components/ContentComponent";
+
 function App() {
   const userInfo = { username: "Admin", isAdmin: true };
   return (
-    <div className="App">
-      <UserInfoContext.Provider value={userInfo}>
-        <BlogPage></BlogPage>
-      </UserInfoContext.Provider>
-    </div>
+    <ThemeProvider>
+      <ContentComponent></ContentComponent>
+    </ThemeProvider>
+
+    // <div className="App">
+    //   <UserInfoContext.Provider value={userInfo}>
+    //     <BlogPage></BlogPage>
+    //   </UserInfoContext.Provider>
+    // </div>
   );
 }
 
